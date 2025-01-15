6742723dd01acb0ed99f9866317114b5f17843f8
basel.31057579@ics.tanta.edu.eg <basel.31057579@ics.tanta.edu.eg>
2025-01-15 13:32:04 +0200

    last update : index localization

--- Tedx/Controllers/HomeController.cs
+++ Tedx/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System.Diagnostics;
 using Tedx.Models;
 
@@ -7,15 +9,24 @@ namespace Tedx.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-		public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , IStringLocalizer<HomeController> localizer)
 		{
 			_logger = logger;
+            _localizer = localizer;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+            // Pass localized strings to the view
+            ViewBag.Date = _localizer["Date"];
+            ViewBag.AttendButton = _localizer["AttendButton"];
+            ViewBag.DaysLabel = _localizer["DaysLabel"];
+            ViewBag.HoursLabel = _localizer["HoursLabel"];
+            ViewBag.MinutesLabel = _localizer["MinutesLabel"];
+            ViewBag.SecondsLabel = _localizer["SecondsLabel"];
+            return View();
 		}
 
 		public IActionResult Privacy()
@@ -38,5 +49,19 @@ namespace Tedx.Controllers
             // Pass the model to the view
             return View(errorViewModel);
         }
+
+
+        [HttpPost]
+        public IActionResult SetCulture(string culture)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect("~/");
+        }
+
     }
 }
